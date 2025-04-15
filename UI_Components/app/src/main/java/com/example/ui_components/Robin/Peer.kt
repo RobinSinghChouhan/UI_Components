9bcf10dc0eab201725ee70ed931b3b7b99327992
Robin Singh Chouhan <robinsingh.02@gmail.com>
2025-04-15 18:45:50 +1000

    Added Files.

--- UI_Components/app/src/main/java/com/example/ui_components/Robin/Peer.kt
+++ UI_Components/app/src/main/java/com/example/ui_components/Robin/Peer.kt
@@ -2,7 +2,7 @@ package com.example.ui_components.Robin
 
 import androidx.compose.ui.graphics.painter.Painter
 
-public class Peer (userId: String, profileImage: Painter, department: String, location: String, follow: Boolean) {
+class Peer (userId: String, profileImage: Painter, department: String, location: String, follow: Boolean) {
 //    val userId = userId
 //    val profileImage = profileImage
 //    val department = department
